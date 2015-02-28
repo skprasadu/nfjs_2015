65c320f6c9f715219531404fa4501723b50558c1
Matt Stine <mstine@gopivotal.com>
2015-02-27 22:58:04 -0500

    spring boot example

---
+++ BuildingTwelveFactorApps/spring-boot/src/main/java/com/mattstine/twelvefactor/springboot/controllers/HelloWorldController.java
@@ -0,0 +1,33 @@
+package com.mattstine.twelvefactor.springboot.controllers;
+
+import com.mattstine.twelvefactor.springboot.core.Saying;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController("/hello-world")
+public class HelloWorldController {
+
+    private final String template;
+    private final String defaultName;
+    private final AtomicLong counter;
+
+    @Autowired
+    public HelloWorldController(@Value("${template}") String template, @Value("${default.name}") String defaultName) {
+        this.template = template;
+        this.defaultName = defaultName;
+        this.counter = new AtomicLong();
+    }
+
+    @RequestMapping(method = RequestMethod.GET, produces = "application/json")
+    public Saying sayHello(@RequestParam(value = "name", required = false) String name) {
+        final String value = String.format(template, (name != null) ? name : defaultName);
+        return new Saying(counter.incrementAndGet(), value);
+    }
+
+}
---
+++ BuildingTwelveFactorApps/spring-boot/src/main/java/com/mattstine/twelvefactor/springboot/health/TemplateHealthCheck.java
@@ -0,0 +1,27 @@
+package com.mattstine.twelvefactor.springboot.health;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TemplateHealthCheck implements HealthIndicator {
+
+    private final String template;
+
+    @Autowired
+    public TemplateHealthCheck(@Value("${template}")String template) {
+        this.template = template;
+    }
+
+    @Override
+    public Health health() {
+        final String saying = String.format(template, "TEST");
+        if (!saying.contains("TEST")) {
+            return Health.down().withDetail("error", "template doesn't include a name").build();
+        }
+        return Health.up().build();
+    }
+}
