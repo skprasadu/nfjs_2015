1ebc73a7c41a17754351da6b1ac7c4094d9ae0e7
Matt Stine <mstine@gopivotal.com>
2015-03-05 23:16:46 -0600

    update to RC3, fix registration bug

--- DistributedSystemsWithSpringCloud/code/consumerFeign/src/main/java/consumerFeign/Application.java
+++ DistributedSystemsWithSpringCloud/code/consumerFeign/src/main/java/consumerFeign/Application.java
@@ -4,12 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.feign.FeignClientScan;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@FeignClientScan
+@EnableFeignClients
 @EnableDiscoveryClient
 @RestController
 public class Application {
